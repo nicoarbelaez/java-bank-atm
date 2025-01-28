246f81af3eab7afbba36235e50f7faf78deaf3c8
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-01-28 15:09:50 -0500

    Class #5 Conceptos Básicos de Java (Variables, Tipos de Datos, Operadores, y prácticas de clean code)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Variables básicas
+        String accountNumber = "123456789";
+        double balance = 1500.75;
+        int pin = 1234;
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Array de montos de transacciones
+        int[] transactionAmounts = {200, -100, 50};
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Operaciones con variables
+        balance += transactionAmounts[0]; // Depósito
+        if (balance > 0 && pin == 1234) {
+            System.out.println("Acceso Concedido");
         }
+
+        // Uso de operadores
+        balance++; // Incremento
+        String status = (balance < 0) ? "Deuda" : "Crédito";
+        System.out.println("Estado de cuenta: " + status);
     }
 }
