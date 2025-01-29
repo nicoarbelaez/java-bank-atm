280e8c8f206290974de190bcb855a1fc28d333cf
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-01-29 11:21:45 -0500

    Add menuAuthentication method in AccountAuthentication interface to handle user authentication

--- src/main/java/org/example/account/AccountAuthentication.java
+++ src/main/java/org/example/account/AccountAuthentication.java
@@ -1,7 +1,11 @@
 package org.example.account;
 
+import java.util.Scanner;
+
 public interface AccountAuthentication {
     
+    void menuAuthentication(Scanner scanner);
+
     boolean authenticate(String name, String password);
     
     boolean isAuthenticated();
