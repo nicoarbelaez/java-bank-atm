4325529eee8c3c96451f7e5022d56304da43744c
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-01-29 13:36:13 -0500

    Implement account creation and sign-in functionality in Main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.example.account.Account;
 import org.example.account.CheckingAccount;
 import org.example.account.InvestimentAccount;
 import org.example.account.SavingsAccount;
+import org.example.enums.AccountType;
 
 public class Main {
 
@@ -14,30 +15,113 @@ public class Main {
 
     public static void main(String[] args) {
         randomData();
+
+        start();
+    }
+
+    public static void start() {
         showWelcomeMessage();
 
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter \"exit\" to close the application.");
+        System.out.println("[1] Sign in to an account");
+        System.out.println("[2] Create account");
+        System.out.print(">> ");
 
+        String option = scanner.nextLine();
+        switch (option) {
+            case "1":
+                signIn(scanner);
+                break;
+            case "2":
+                signUp(scanner);
+                break;
+            case "exit":
+                System.out.println("Goodbye!");
+                break;
+            default:
+                start();
+                break;
+        }
+        scanner.close();
+    }
+
+    public static void signIn(Scanner scanner) {
         String accountNumber = "";
         Account account = null;
         do {
             if (accountNumber != "") {
+                showWelcomeMessage();
                 System.out.println("Account not found. Please try again.");
             }
             System.out.println("Enter your account number:");
 
             accountNumber = scanner.nextLine();
 
-            if (accountNumber.equals("exit")) {
-                break;
-            }
-
             account = Util.findAccount(accounts, accountNumber);
         } while (account == null);
 
-        scanner.close();
+        account.menuAuthentication(scanner);
+    }
+
+    public static void signUp(Scanner scanner) {
+        System.out.println("Account type:");
+
+        AccountType[] accountType = AccountType.values();
+        for (int i = 0; i < accountType.length; i++) {
+            System.out.println("[" + (i + 1) + "] " + Util.capitalizeFirstLetter(accountType[i].toString()));
+        }
+        System.out.print(">> ");
+
+        String typeAccountOption = scanner.nextLine();
+
+        if (typeAccountOption.equals("exit")) {
+            start();
+            return;
+        }
+
+        if (!Util.isNumeric(typeAccountOption)) {
+            System.out.println("Invalid option. Please try again.");
+            signUp(scanner);
+            return;
+        }
+
+        int parseIntOption = Integer.parseInt(typeAccountOption);
+        if (parseIntOption < 1 || parseIntOption > AccountType.values().length) {
+            System.out.println("Invalid option. Please try again.");
+            signUp(scanner);
+            return;
+        }
+
+        System.out.print("Enter your name:");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter your password:");
+        String password = scanner.nextLine();
+
+        System.out.print("Enter your balance:");
+        double balance = scanner.nextDouble();
+
+        createAccount(name, password, balance, AccountType.values()[parseIntOption - 1]);
+
+        Account account = accounts.get(accounts.size() - 1);
+        System.out.println("Account created successfully!");
+        System.out.println("Your account number is: " + account.getAccountNumber());
+    }
+
+    public static void createAccount(String name, String password, double balance, AccountType accountType) {
+        switch (accountType) {
+            case SAVINGS:
+                accounts.add(new SavingsAccount(name, password, balance));
+                break;
+            case CHECKING:
+                accounts.add(new CheckingAccount(balance));
+                break;
+            case INVESTMENT:
+                accounts.add(new InvestimentAccount(balance));
+                break;
+        }
     }
 
     public static void randomData() {
@@ -53,17 +137,7 @@ public class Main {
             String name = "Account " + (i + 1);
             String password = "123" + (char) Util.generateRandomInt(65, 122);
 
-            switch (Util.getRandomAccountType()) {
-                case SAVINGS:
-                    accounts.add(new SavingsAccount(name, password, balance));
-                    break;
-                case CHECKING:
-                    accounts.add(new CheckingAccount(balance));
-                    break;
-                case INVESTMENT:
-                    accounts.add(new InvestimentAccount(balance));
-                    break;
-            }
+            createAccount(name, password, balance, Util.getRandomAccountType());
         }
     }
 
